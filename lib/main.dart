781211b7e9e4b5221b9ae5522be3ca5ca64d697d
AshishLawaju <ashish.lawaju74@gmail.com>
2022-11-28 18:10:52 +0545

    createRaisedButtom

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Quiz App')),
-      body:Container(child: ,);
-      
-
-    
+    var ques = ['what\'s your favorite color', 'what\'s you favorite animal'];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz App'),
+      ),
+      body: Column(
+        children: [
+          Text("The question !"),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('answer 1'),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Answer2'),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RaisedButton(onPressed: () {}, child: Text('answer3'))
+        ],
+      ),
+    ));
   }
 }
+// 
